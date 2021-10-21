6bfc1391c36592588020fe21cd0ba3241ae809d3
Solovyeva Evgenia <joeweird@gmail.com>
2021-10-21 12:45:13 +0500

    Page offset fixed

--- BlogEngine/src/main/java/main/service/ApiPostService.java
+++ BlogEngine/src/main/java/main/service/ApiPostService.java
@@ -23,31 +23,23 @@ public class ApiPostService {
     public ApiPostListResponse getPosts(int offset, int limit, String mode) {
         ApiPostListResponse apiList = new ApiPostListResponse();
         List<Post> posts = new ArrayList<>();
-        Pageable pageable = PageRequest.of(offset, limit);
+        Pageable pageable = PageRequest.of(offset / limit, limit);
+        Page<Post> page;
         switch (mode) {
-            case "recent":
-                Page<Post> recentPage = postRepository.findRecentPosts(pageable);
-                posts.addAll(recentPage.getContent());
-                apiList.setCount(recentPage.getTotalElements());
-                break;
             case "popular":
-                Page<Post> popularPage = postRepository.findPopularPosts(pageable);
-                posts.addAll(popularPage.getContent());
-                apiList.setCount(popularPage.getTotalElements());
+                page = postRepository.findPopularPosts(pageable);
                 break;
             case "early":
-                Page<Post> earlyPage = postRepository.findEarlyPosts(pageable);
-                posts.addAll(earlyPage.getContent());
-                apiList.setCount(earlyPage.getTotalElements());
+                page = postRepository.findEarlyPosts(pageable);
                 break;
             case "best":
-                Page<Post> bestPage = postRepository.findBestPosts(pageable);
-                posts.addAll(bestPage.getContent());
-                apiList.setCount(bestPage.getTotalElements());
+                page = postRepository.findBestPosts(pageable);
                 break;
             default :
-                apiList.setCount(0);
+                page = postRepository.findRecentPosts(pageable);
         }
+        posts.addAll(page.getContent());
+        apiList.setCount(page.getTotalElements());
         List<PostDto> postDtoList = posts.stream().map(mapperService::convertPostToDto)
                 .collect(Collectors.toList());
         apiList.setPosts(postDtoList);
